83e79573e548e84a7742f6114e28e5ef4e191fa6
achaari jaouad <achaari.jaouad@gmail.com>
2025-05-16 03:14:33 +0100

    ste 1 - challenge 5

--- set1_Basics/challenge5/main.go
+++ set1_Basics/challenge5/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
-func main() {
-    fmt.Println("Challenge placeholder")
+func xorRepeatingKey(plaintext, key []byte) []byte {
+	cipher := make([]byte, len(plaintext))
+
+	for i := 0; i < len(plaintext); i++ {
+		cipher[i] = plaintext[i] ^ key[i%len(key)]
+	}
+	return cipher
 }
 
+var input = `Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`
+
+func main() {
+	plaintext := []byte(input)
+	key := []byte("ICE")
+
+	cipher := xorRepeatingKey(plaintext, key)
+	fmt.Println("Ciphertext:", hex.EncodeToString(cipher))
+}
