7d56e724b5ed090c213938587b1e09f206bda15e
achaari jaouad <achaari.jaouad@gmail.com>
2025-05-16 02:24:46 +0100

    ste 1 - challenge 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cryptopals_challenges
+
+## Set 1
+### challenge 1 : Convert hex to base64
--- set1_Basics/challenge1/main.go
+++ set1_Basics/challenge1/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Challenge placeholder")
-}
+	hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	decodedHex, _ := hex.DecodeString(hexStr)
+	b64 := base64.StdEncoding.EncodeToString(decodedHex)
 
+	fmt.Println(string(b64))
+}
