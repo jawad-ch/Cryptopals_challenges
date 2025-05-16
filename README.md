2618b7784fd2cd6f69b9d03a7e4ce00fdc45ef68
achaari jaouad <achaari.jaouad@gmail.com>
2025-05-16 02:36:47 +0100

    ste 1 - challenge 2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Set 1
 ### challenge 1 : Convert hex to base64
+### challenge 2 : Fixed XOR
--- set1_Basics/challenge2/main.go
+++ set1_Basics/challenge2/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
-func main() {
-    fmt.Println("Challenge placeholder")
+func xorEncrypt(plaintext, key []byte) []byte {
+	cipher := make([]byte, len(plaintext))
+
+	for i := 0; i < len(plaintext); i++ {
+		cipher[i] = plaintext[i] ^ key[i] // Key repeats if shorter
+	}
+	return cipher
 }
 
+func main() {
+	buff1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	buff2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+
+	cph := xorEncrypt(buff1, buff2)
+
+	fmt.Println("Ciphertext:", hex.EncodeToString(cph))
+}
