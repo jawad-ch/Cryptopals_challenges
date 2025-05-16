3f341295af6f606b74d561d66d1b38c44c08b007
achaari jaouad <achaari.jaouad@gmail.com>
2025-05-17 00:54:43 +0100

    ste 1 - challenge 7

--- set1_Basics/challenge7/main.go
+++ set1_Basics/challenge7/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"crypto/aes"
+	"encoding/base64"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Challenge placeholder")
+	ciphertext, err := os.ReadFile("src/file.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(string(ciphertext))
+	if err != nil {
+		fmt.Println("Error decoding base64:", err)
+		return
+	}
+
+	key := []byte("YELLOW SUBMARINE")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println("Error creating cipher:", err)
+		return
+	}
+
+	plaintext := make([]byte, len(decoded))
+
+	// Decrypt block by block (16 bytes each)
+	for start := 0; start < len(decoded); start += 16 {
+		block.Decrypt(plaintext[start:start+16], decoded[start:start+16])
+	}
+
+	fmt.Println(string(plaintext))
 }
