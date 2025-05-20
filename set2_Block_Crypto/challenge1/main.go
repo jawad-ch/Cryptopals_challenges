d8384415e52b3eaedac7c1bf286e0378c896876a
achaari jaouad <achaari.jaouad@gmail.com>
2025-05-20 23:23:02 +0100

    ste 2 - challenge 1

--- set2_Block_Crypto/challenge1/main.go
+++ set2_Block_Crypto/challenge1/main.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-    fmt.Println("Challenge placeholder")
+// PKCS#7 padding
+func pkcs7Pad(data []byte, blockSize int) []byte {
+	padding := blockSize - (len(data) % blockSize)
+	padtext := make([]byte, padding)
+	for i := 0; i < padding; i++ {
+		padtext[i] = byte(padding)
+	}
+	return append(data, padtext...)
 }
 
+func main() {
+	src := "YELLOW SUBMARINE"
+	padded := pkcs7Pad([]byte(src), 16) // will padd 16 bytes of 16
+	fmt.Println(padded)
+}
